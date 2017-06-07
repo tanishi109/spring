73a35761b23aad3f2f6c03a74c375b83a3cd9a92
tanishi109 <hayakawa@goodpatch.com>
2017-06-07 22:45:35 +0900

    export setting

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/page1": { page: "/page1" },
+      "/page2": { page: "/page2" },
+      "/page3": { page: "/page3" },
+      "/page4": { page: "/page4" },
+      "/page5": { page: "/page5" },
+      "/page6": { page: "/page6" },
+    }
+  },
+}
