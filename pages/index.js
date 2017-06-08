7578c0bd97720c592eb6193c7fca1e87fa5b7a09
tanishi109 <hayakawa@goodpatch.com>
2017-06-08 20:54:23 +0900

    add links

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,36 @@ import Canvas from "../components/Canvas"
 
 export default () => (
   <div>
+    <div>
+      <a href="page1">page1</a>
+    </div>
+
+    <div>
+      <a href="page2">page2</a>
+    </div>
+
+    <div>
+      <a href="page3">page3</a>
+    </div>
+
+    <div>
+      <a href="page4">page4</a>
+    </div>
+
+    <div>
+      <a href="page5">page5</a>
+    </div>
+
+    <div>
+      <a href="page6">page6</a>
+    </div>
+
     <Canvas />
+
+    <style jsx>{`
+      div {
+        margin: 8px;
+      }
+    `}</style>
   </div>
 )
